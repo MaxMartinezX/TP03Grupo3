f3518e9cafd3b80173e68f904e91d33e6f0ffa3a
paintx <maxipro328@gmail.com>
2023-04-30 17:01:47 -0300

    Proyecto Vacio - Martinez Maximiliano Pueden comenzar a hacer su parte :D

---
+++ src/main/java/ar/edu/unju/edm/Tp03Grupo3Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp03Grupo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp03Grupo3Application.class, args);
+	}
+
+}
